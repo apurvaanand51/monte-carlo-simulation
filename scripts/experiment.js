99b3183f872486d2c6c91d915f95dae27588137c
Apurva Anand <apurvaanand51@gmail.com>
2026-03-10 02:20:34 +0530

    logic fix

--- scripts/experiment.js
+++ scripts/experiment.js
@@ -1,7 +1,7 @@
 import { simulate, probabilityHistory, experimentResults } from "./simulation.js";
 import { drawDistribution, drawConvergence } from "./visualization.js";
 
-const result = simulate(10, 100);
+const result = simulate(10, 1000);
 drawDistribution(experimentResults);
 drawConvergence(probabilityHistory);
 console.log(result);
