f3d2fa575345a52bd1e875011353e2e4f6741c88
Apurva Anand <apurvaanand51@gmail.com>
2026-03-10 02:27:51 +0530

    increased the number of flips

--- scripts/experiment.js
+++ scripts/experiment.js
@@ -1,7 +1,7 @@
 import { simulate, probabilityHistory, experimentResults } from "./simulation.js";
 import { drawDistribution, drawConvergence } from "./visualization.js";
 
-const result = simulate(10, 1000);
+const result = simulate(100, 1000);
 drawDistribution(experimentResults);
 drawConvergence(probabilityHistory);
 console.log(result);
